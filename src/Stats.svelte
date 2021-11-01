8ff30bdf46b4d53c7c53df459a366a2cb8a0759e
LUKE PLOURDE <lukeplourde@hotmail.com>
2021-11-01 11:08:20 -0400

    Search added to filter through teams

--- src/Stats.svelte
+++ src/Stats.svelte
@@ -1,18 +1,43 @@
 <script>
 let stats = getStats();
-
+let search;
+// let index = -1;
    async function getStats(){
         const res = await fetch("https://statsapi.web.nhl.com/api/v1/standings?hydrate=record(overall),division,conference,team(nextSchedule(team),previousSchedule(team))&season=20212022&site=en_nhl")
         const stats = await res.json();
+        // if(search && search.length > 0){
+        //    index = stats.records.findIndex(division => {
+        //         return division.teamRecords
+        //             .find(team => team.team.name.toLowerCase().includes(search.toLowerCase())) ? 
+        //             true : false
+ 
+        //     });
+        //     console.log(index);
+        // }
         return stats;
     }
 
+ function getIndex(){
 
+ }
 </script>
- <button on:click={getStats}>Get Stats</button>
 
+    <input bind:value={search} type="text">
 {#await stats}
     loading
-{:then value}
-    <h1>{value.records[0].teamRecords[0].team.name)}</h1>
+{:then response}
+
+     {#each response.records as division}
+ 
+        <h1>Division: {division.division.name}</h1>
+        {#each division.teamRecords as team}
+            {#if !search || team.team.name.toLowerCase().includes(search.toLowerCase())}
+            Team: {team.team.name}
+            Wins: {team.leagueRecord.wins}
+            Losses: {team.leagueRecord.losses}
+            OT: {team.leagueRecord.ot}
+            <br> 
+            {/if}
+        {/each}
+     {/each}
 {/await}
