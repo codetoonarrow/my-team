17bbc628250c9d19c87498b07c91c573e67445d5
LUKE PLOURDE <lukeplourde@hotmail.com>
2022-06-07 22:03:47 -0300

    Testing which NHL API should hit to get the results of the games and settled on a URL to fetch from that will get all of the info that I will need

--- src/Wins.svelte
+++ src/Wins.svelte
@@ -1,7 +1,7 @@
 <script>
 let dateString
 let wins = getWins()
-
+let i = 0
 async function getWins(){
     const res = await fetch("https://statsapi.web.nhl.com/api/v1/people/8471698/stats?stats=gameLog&season=20212022")
     const wins = await res.json()
@@ -22,11 +22,16 @@ function handleClick(){
 {:then response} 
     {#each response.stats as stats}
          {#each stats.splits as winner}
-              <h1>{winner.date}</h1>
+              <!-- <h1>{winner.date}</h1>
               <h1>{winner.team.name}</h1>
               <h1>{winner.opponent.name}</h1>
               <h1>{winner.isWin}</h1>
-        
+              <h1>{winner.isOT}</h1> -->
+            {#if winner.isWin === true|| winner.isOT === true}
+                 <h1>{winner.team.name}</h1>
+                 {:else}
+                     <h1>{winner.opponent.name}</h1>
+            {/if}
          {/each}
     {/each}
 {/await}
