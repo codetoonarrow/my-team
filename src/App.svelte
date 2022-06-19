b56b5ede01aff4a624e09b0367b34b075f195036
LUKE PLOURDE <lukeplourde@hotmail.com>
2022-06-19 18:24:51 -0300

    Add grungy background photo

--- src/App.svelte
+++ src/App.svelte
@@ -5,8 +5,18 @@
 </script>
 
 <main>
+	<!-- svelte-ignore a11y-img-redundant-alt -->
+	<img src="../Grungy03-byGhostlyPixels.png" alt="background image" />
 	<Header />
 	<Stats />
 	<Footer />
 </main>
 
+<style>
+img {
+	position:absolute;
+	z-index: -100;
+	height: 100%;
+	width: 100%;
+}
+</style>
