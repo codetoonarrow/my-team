ef2e6e5ea8dfba47b5aafc653ac1a673bf46e698
LUKE PLOURDE <lukeplourde@hotmail.com>
2022-06-27 20:53:39 -0300

    Style Modal chart

--- src/Chart.svelte
+++ src/Chart.svelte
@@ -5,32 +5,29 @@ export let outcome;
 //For both outcomes 1st point +30
 
 </script>
-<svg class="chart" width="300" height="100">
-	<!-- x axis -->
-	<line x1="0" x2="300" y1="100" y2="100"></line>
-	<g class="x" transform="translate(0,120)">
-		<text x="0">0</text>
-		<text x="60">2021-10-21</text>
-		<text x="120">4</text>
-		<text x="180">6</text>
-		<text x="240">8</text>
-		<text x="300">10</text>
-	</g>
-	
-	<!-- y axis -->
-	<line x1="0" x2="0" y1="0" y2="100"></line>
-	<g class="y" transform="translate(-10,0)">
-		<text y="100">Loss</text>
-		<text y="0">Win</text>
-	</g>
-	
-	<!-- data -->
-	
-	<polyline style="stroke: red; stroke-width: 2" points={outcome}></polyline>
-
-
-</svg>
-
+<div class="chart">
+	<svg width="300" height="100">
+		<!-- x axis -->
+		<line x1="0" x2="300" y1="100" y2="100"></line>
+		<g class="x" transform="translate(0,120)">
+			<text x="0">0</text>
+			<text x="60">2021-10-21</text>
+			<text x="120">4</text>
+			<text x="180">6</text>
+			<text x="240">8</text>
+			<text x="300">10</text>
+		</g>
+		
+		<!-- y axis -->
+		<line x1="0" x2="0" y1="0" y2="100"></line>
+		<g class="y" transform="translate(-10,0)">
+			<text y="100">Loss</text>
+			<text y="0">Win</text>
+		</g>
+		<!-- data -->
+		<polyline style="stroke: red; stroke-width: 2" points={outcome}></polyline>
+	</svg>
+</div>
 <style>
 	svg {
 		overflow: visible;
@@ -51,6 +48,12 @@ export let outcome;
 	.y text {
 		text-anchor: end;
 		dominant-baseline: middle;
+	}
+
+	.chart {
+	display: flex;
+	align-items: center;
+	justify-content: center;
 	}
 
 </style>
