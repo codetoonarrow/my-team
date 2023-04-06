c908061bd5d731910acf2678ba25753bbb550bb6
LUKE PLOURDE <lukeplourde@hotmail.com>
2023-04-06 17:48:41 -0300

    Adding comments to the modal comp

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -1,23 +1,25 @@
 <script>
+ 
+    import Chart from './Chart.svelte';
+
     import { fly, fade } from 'svelte/transition';
     import { createEventDispatcher } from 'svelte';
-    import Chart from './Chart.svelte';
-    import YearSelect from './YearSelect.svelte';
+
     import { dataset_dev } from 'svelte/internal';
     export let teamName;
+    export let id;
     // export let rank;
     // export let points;
-    export let id;
     let wins = getWins();
     let gameResults = []
     let yAxis = 0
-    let selectedYear
-    let items = [2021, 2022, 2023]
-    let answer = ''
+    let selectedYear = 2018
+    let items = [2018, 2019, 2020, 2021]
 
-    async function getWins(){
-        let season = 20212022
-        const res = await fetch(`https://statsapi.web.nhl.com/api/v1/schedule?season=${season}`)
+
+    async function getWins(combinedSeasonYear = 20182019){
+        console.log("Ran in getWins func" + " " + combinedSeasonYear)
+        const res = await fetch(`https://statsapi.web.nhl.com/api/v1/schedule?season=${combinedSeasonYear}`)
         const wins = await res.json()
         const results = wins.dates
         for (let i = 0; i  < results.length; i++){
@@ -25,18 +27,27 @@
             for (let j = 0; j < obj.length; j++){
                 if (obj[j].teams.home.team.id === id && obj[j].teams.home.score > obj[j].teams.away.score || obj[j].teams.away.team.id === id && obj[j].teams.away.score > obj[j].teams.home.score) {
                     updateOutcome("WIN")
-                }else if (obj[j].teams.home.team.id === id && obj[j].teams.home.score < obj[j].teams.away.score|| obj[j].teams.away.team.id === id && obj[j].teams.away.score < obj[j].teams.home.score){
+                }else if (obj[j].teams.home.team.id === id && obj[j].teams.home.score < obj[j].teams.away.score || obj[j].teams.away.team.id === id && obj[j].teams.away.score < obj[j].teams.home.score){
                     updateOutcome("LOSS")
                 }
             }
         }
     }
+
+    // The NHL API requires that if a season is to be returned it needs to be the year and the year following
+    // For example: Season of 2018 would be "20182019"
+    // This function takes a selected year as a string and appends the next year to the
     function seasonYear(selectedYear){
         let seasonYearStart = selectedYear
         let seasonYearEnd = selectedYear + 1
-        let combinedSeasonYear = seasonYearStart + seasonYearEnd
-        console.log(combinedSeasonYear)
+        let addSeason = seasonYearStart.toString() + seasonYearEnd.toString()
+        let combinedSeasonYear = Number(addSeason)
+        if(combinedSeasonYear > 0 ){
+            console.log("this season is" + combinedSeasonYear)
+            return getWins(combinedSeasonYear)
+        }   
     }
+    
     function updateOutcome(linePoint){
         let result
         let finalResult
@@ -69,26 +80,26 @@
 
 <div class="modal-bg" transition:fade>
     <div class="modal" transition:fly={{ y: -100 }}>
-        <h2 class="team-name">{teamName}</h2>
+        <h1 class="team-name">{teamName}</h1>
         <img class="team-logo" src="https://www-league.nhlstatic.com/images/logos/teams-current-primary-light/{id}.svg" alt="{teamName} Logo">
         <!-- <h3>League Rank: {rank}</h3>
         <h3>Points: {points}</h3> -->
         {#await wins}
             loading
         {:then}
-            <Chart outcome={gameResults}/>
+            <h3>{selectedYear} Season</h3>
+            <Chart outcome={gameResults} />
         {/await}
         <button on:click={ () => {
             dispatch('close');
         }}
         
         >Close</button>  
-        <select bind:value={selectedYear} on:change="{() => answer = ''}">
+        <select bind:value={selectedYear} on:change="{seasonYear(selectedYear)}">
             {#each items as item}
                 <option value={item}>{item}</option>
-                
             {/each}
-        </select>  
+        </select>   
     </div>
 </div>
 
