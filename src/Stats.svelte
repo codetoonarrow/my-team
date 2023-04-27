ccf5f3ffd84cfce12e8b37251002c3af14b648dc
LUKE PLOURDE <lukeplourde@hotmail.com>
2023-04-27 14:38:06 -0400

    Feat: Adding LocalStorage to the app

--- src/Stats.svelte
+++ src/Stats.svelte
@@ -1,13 +1,14 @@
 <script>
 // https://statsapi.web.nhl.com/api/v1/schedule?season=20212022
 // API for past schedule
+import { onMount } from 'svelte';
 import { get, writable } from 'svelte/store';
 import Card from './Team-Card.svelte';
 import DivisionCard from './DivisionCard.svelte';
 import Modal from './Modal.svelte';
 import Showcase from './Showcase.svelte';
 
-let stats = getStats()
+// let stats = getStats()
 let modalIsOpen = false
 let search
 let teamName
@@ -15,61 +16,70 @@ let rank
 let points
 let id
 let scroll
+let stats = []
 
-//Todo: Match random id to team and display stats related to team
+const fetchData = async () => {
+    const response = await fetch("https://statsapi.web.nhl.com/api/v1/standings?hydrate=record(overall),division,conference,team(nextSchedule(team),previousSchedule(team))&season=20212022&site=en_nhl")
+    stats = await response.json()
+    localStorage.setItem("stats", JSON.stringify(stats))
+}
 
-   async function getStats(){
-        const res = await fetch("https://statsapi.web.nhl.com/api/v1/standings?hydrate=record(overall),division,conference,team(nextSchedule(team),previousSchedule(team))&season=20212022&site=en_nhl")
-        const stats = await res.json();
-        // getTeamIds(stats)
-        return stats;
+onMount(() => {
+    const storedData = localStorage.getItem("stats")
+    if (storedData) {
+        stats = JSON.parse(storedData)
+        console.log("this is the local storage " + stats)
+    } else {
+        fetchData()
     }
+})
+
+//    async function getStats(){
+//         const res = await fetch("https://statsapi.web.nhl.com/api/v1/standings?hydrate=record(overall),division,conference,team(nextSchedule(team),previousSchedule(team))&season=20212022&site=en_nhl")
+//         const stats = await res.json();
+//         return stats;
+//     }
 
     let teamIds = []
     function getTeamIds(stats){
-          
         for(let i = 0; i < stats.records.length; i++){
             for(let j = 0; j < stats.records[i].teamRecords.length; j++){
                 const teamId = stats.records[i].teamRecords[j].team.id;
             if(!teamIds.includes(teamId)){
-                console.log(stats.records[i].teamRecords[j].team.id);
                 teamIds.push(teamId);
-                
             }
             }
         }
-        console.log(teamIds);
         return teamIds
     }
 
     async function generateRandomid(){
-        const stats = await getStats()
+        const stats = await fetchData()
         const teamIds = getTeamIds(stats)
         const randomNumber = Math.floor(Math.random() * teamIds.length);
-        return teamIds[randomNumber]
+        const randomTeamID = teamIds[randomNumber]
+        console.log("This is the random team ID " + randomTeamID)
+        return randomTeamID
     }
 
+    // async function getRandomTeamDetails(randomTeamID) {
+    //     const stats = await getStats()
+    //     for(let i = 0; stats.records.length; i++ ){
+    //         const teamId = stats.records[i].teamRecords[j].team.id
+    //         for(let j = 0; j < stats.records[i].teamRecords.length; j++){
+    //             if(teamId === randomTeamID) {
+    //                 console.log(stats.records[i].teamRecords[j].team.name)
+    //             }
+    //         }
+    //     }
+    // }
   
     let result = writable(null);
 
-    //Generate Random ID number
-    //If the number is equal to an ID from the API return the team name 
-//     function checkId(stats){
-//         console.log(stats)
-//         for(let i = 0; i < stats.records.length; i++){
-//             for(let j = 0; j < stats.records[i].teamRecords.length; j++){
-//                 if (result === stats.records[i].teamRecords[j].team.name) {
-//                     console.log(stats.records[i].teamRecords[j].team.name)
-//                 }
-//             }
-//         }
-//     }
-// checkId(stats)
         generateRandomid().then((randomTeamId) =>{
-            result.set(randomTeamId)
+            result.set(randomTeamId) 
     })
-
- 
+     
     function closeModal(){
         modalIsOpen = false
     }
