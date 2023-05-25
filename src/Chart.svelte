802599c0e861ef07f551ff74edb208d9d0a0f917
LUKE PLOURDE <lukeplourde@hotmail.com>
2023-05-24 21:24:36 -0400

    Feat: Download SVG Chart

--- src/Chart.svelte
+++ src/Chart.svelte
@@ -1,20 +1,51 @@
+<script>
+export let outcome;
+</script>
+
 <script context="module">
  
+let svgRef;
 
 export function downloadSVG(){
-	console.log("hello")
-}
+    const svg = svgRef;
+    const svgData = new XMLSerializer().serializeToString(svg);
+    const blob = new Blob([svgData], { type: 'image/svg+xml' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.download = 'download.svg';
+    a.href = url;
+    a.click();
+    URL.revokeObjectURL(url);
+	}
 
 </script>
 
-<script>
-
-export let outcome;
-
-</script>
 
 <div class="chart">
-	<svg  width="600" height="100" role="img">
+	<svg bind:this={svgRef} width="600" height="100" role="img">
+		<!-- In order for the CSS styles to be preserved the original color and styles-->
+		<style>
+			svg {
+				overflow: visible;
+				margin: 3em;
+			}
+
+			line, polyline {
+				fill: none;
+				stroke: black;
+			}
+			
+			.y text {
+				text-anchor: end;
+			}
+		
+			.chart {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			}
+		</style>
+
 		<!-- x axis -->
 		<line x1="0" x2="600" y1="100" y2="100"></line>
 		<g class="x" transform="translate(0,120)"></g>
@@ -29,27 +60,3 @@ export let outcome;
 		<polyline style="stroke: #ef6351; stroke-width: 3" points={outcome}></polyline>
 	</svg>
 </div>
-
-<style>
-	svg {
-		overflow: visible;
-		margin: 3em;
-	}
-
-
-	line, polyline {
-		fill: none;
-		stroke: black;
-	}
-	
-	.y text {
-		text-anchor: end;
-	}
-
-	.chart {
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	}
-
-</style>
